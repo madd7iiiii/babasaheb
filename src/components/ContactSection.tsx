b6cf6c46de19e17fa33e602349eea0bff4600686
Gaurav Thavkar <gauravthavkar9@gmail.com>
2026-02-08 18:54:53 +0530

    added google map location

--- src/components/ContactSection.tsx
+++ src/components/ContactSection.tsx
@@ -69,11 +69,11 @@ const ContactSection = () => {
             </div>
           </div>
 
-          {/* Map placeholder */}
+          {/* Map placeholder - ✅ UPDATED LINK HERE */}
           <div className="bg-card rounded-xl border border-border shadow-sm overflow-hidden">
             <iframe
               title="शाळेचा नकाशा"
-              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d29873.123!2d78.84!3d20.56!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bd307f0b8e9e8c7%3A0x1b0c2b7b1e7e7e7e!2sHinganghat%2C%20Maharashtra!5e0!3m2!1sen!2sin!4v1700000000000!5m2!1sen!2sin"
+              src="https://maps.google.com/maps?q=Bharatratna%20Dr.%20Babasaheb%20Ambedkar%20Matimand%20Mula-Mulinchi%20Niwasi%20Vishesh%20Shala%20Hinganghat&t=&z=15&ie=UTF8&iwloc=&output=embed"
               width="100%"
               height="100%"
               style={{ border: 0, minHeight: "400px" }}
